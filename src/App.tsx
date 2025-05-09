68458e81f5844f4e1fac7d543808a71795c1a197
Marco <marcocronje@gmail.com>
2025-05-09 11:51:20 +0200

    improving project detail page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BackgroundPattern } from "@/components/BackgroundPattern";
 
 import IndexPage from "@/pages/index";
 import ProjectsPage from "@/pages/projects";
+import ProjectDetailPage from "@/pages/project-detail"; // Add this import
 import SkillsPage from "@/pages/skills";
 import ExperiencePage from "@/pages/experience";
 import AboutPage from "@/pages/about";
@@ -12,15 +13,13 @@ import ContactPage from "@/pages/contact";
 function App() {
   return (
     <>
-      {/* Theme loader injects fonts and applies global styles */}
       <ThemeLoader />
-      
-      {/* Background pattern */}
       <BackgroundPattern />
       
       <Routes>
         <Route element={<IndexPage />} path="/" />
         <Route element={<ProjectsPage />} path="/projects" />
+        <Route element={<ProjectDetailPage />} path="/projects/:id" /> {/* Add this route */}
         <Route element={<SkillsPage />} path="/skills" />
         <Route element={<ExperiencePage />} path="/experience" />
         <Route element={<AboutPage />} path="/about" />
